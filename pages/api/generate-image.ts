ded2c1e1329901b116122b97beed2702fa5cb537
Rocco Maniscalco <roccomaniscalco2001@gmail.com>
2022-11-07 22:56:13 -0500

    improve type safety

---
+++ pages/api/generate-image.ts
@@ -0,0 +1,28 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next"
+import { Configuration, ImagesResponse, OpenAIApi } from "openai"
+
+const configuration = new Configuration({
+  organization: process.env.OPENAI_ORG_ID,
+  apiKey: process.env.OPENAI_API_KEY,
+})
+const openai = new OpenAIApi(configuration)
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ImagesResponse | string>
+) {
+  try {
+    const response = await openai.createImage({
+      prompt: req.body.prompt,
+      size: "1024x1024",
+      response_format: "url",
+      n: 1,
+      user: undefined,
+    })
+
+    res.status(200).json(response.data)
+  } catch (error) {
+    res.status(500).json("Failed to generate image")
+  }
+}
